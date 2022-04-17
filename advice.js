e919f99b46eb261977c9bfce6c4dba63c39f72d2
FedericoSanchez-Ksquare <102171026+FedericoSanchez-Ksquare@users.noreply.github.com>
2022-04-17 12:09:21 -0500

    Button now shows a new advice
    beware of mozilla, activate the ignore cache or something like that to work or test it on chrome

--- advice.js
+++ advice.js
@@ -11,6 +11,14 @@ const getAdviceOnLoad = () => {
     });
 };
 
+const getAdvice = () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      printOnScreen(pText, aSelec, data);
+    });
+};
+
 const printOnScreen = async (pText, aSelec, data) => {
   pText.innerHTML = `"${data.slip.advice}"`;
 };
